93cd7a5998e42884013c83cc5a1e3e91d959d65b
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-08-04 23:00:23 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,5 @@
+A=float(input("Enter the first number: "))
+B=float(input("Enter the second number: "))
+C=float(input("Enter the third number: "))
+Average=(A+B+C)/3
+print("the average is:" ,Average)
