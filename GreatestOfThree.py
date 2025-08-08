7bb4ec19fae33654acdc940e2840acaf8757fd18
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-08-08 22:59:01 +0530

    Add files via upload

---
+++ GreatestOfThree.py
@@ -0,0 +1,11 @@
+A=int(input("Enter the first number: "))
+B=int(input("Enter the second number: "))
+C=int(input("Enter the third number: "))
+if(A>B&B>C):
+    print("A is the greatest.",A)
+elif(B>A&A>C):
+    print("B is the greatest.",B)
+else:
+    print("C is the greatest.",C)
+    
+
