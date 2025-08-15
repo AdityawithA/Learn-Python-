f0489d48a9878c4b373a17550ebdb46701f129f6
ADITYA KUMAR <145112088+AdityawithA@users.noreply.github.com>
2025-08-15 20:25:53 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,8 @@
+a=input("Enter the first favourite movie name: ")
+b=input("Enter the second favourite movie name: ")
+c=input("Enter the third favourite movie name: ")
+print(a)
+print(b)
+print(c)
+list=[a,b,c]
+print(list)
